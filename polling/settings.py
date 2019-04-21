116006bcc00aefde6dd49cb7b57082119f0b3622
Aizaz Ansari <aizazarifansari@gmail.com>
2019-04-21 21:49:43 +0200

    updated after long time sry

--- polling/settings.py
+++ polling/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '8lm_-+v5le@@%a@cqw!1jhmc5u0)xf$2&i9=1cxf+-fa)1@yn#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
